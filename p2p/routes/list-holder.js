3c9a55b8c672748ee660079d374accffc55e5fb3
Tanner Braun <tsbraun1891@gmail.com>
2020-06-12 13:17:34 -0400

    Add swagger documentation and UI

--- p2p/routes/list-holder.js
+++ p2p/routes/list-holder.js
@@ -12,19 +12,86 @@ const csvStringifier = createCsvStringifier({
   header: [
     { id: 'ssn', title: 'SSN' },
   ]
-})
+});
 
-router.put('/listname/:fileName', (req, res, next) => {
-  fileName = req.params.fileName + '.csv';
-  if(req.body.encodeType) encodeType = req.body.encodeType;
+/**
+ * @swagger
+ * tags:
+ *   name: Update List
+ *   description: Updating and editing private list
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /listholder/listname:
+ *    put:
+ *      summary: Change the active list file
+ *      tags: [Update List]
+ *      parameters:
+ *       - in: body
+ *         name: FileName-Update
+ *         schema:
+ *           type: object
+ *           required:
+ *            - fileName
+ *           properties:
+ *             fileName:
+ *               type: string
+ *               description: Name of the file to change to the active file without extension
+ *             encodeType:
+ *               type: string
+ *               description: Whether to encode data being stored in 'ASCII' or 'UTF-8'
+ *           example:
+ *             input: "test-table"
+ *             encodeType: "ASCII"
+ *      responses:
+ *        "200":
+ *          description: Active file was successfully changed
+ *          content:
+ *            text/plain:
+ *              schema:
+ *                type: string
+ *                example: "File changed to test-table successfully"
+ */
+router.put('/listname/', (req, res, next) => {
+  fileName = req.body.fileName + '.csv';
+  if (req.body.encodeType) encodeType = req.body.encodeType;
 
   res.status(200).send("File changed to " + fileName + " successfully");
 });
 
-// send object { input: SSNum }
+/**
+ * @swagger
+ * path:
+ *  /listholder/singleUpdate:
+ *    post:
+ *      summary: Add a single entry to the private list
+ *      tags: [Update List]
+ *      parameters:
+ *       - in: body
+ *         name: Single-Update
+ *         schema:
+ *           type: object
+ *           required:
+ *            - input
+ *           properties:
+ *             input:
+ *               type: string
+ *               description: Value to be masked and appended to list
+ *             encodeType:
+ *               type: string
+ *               description: Whether to encode data being stored in 'ASCII' or 'UTF-8'
+ *           example:
+ *             input: "123456789"
+ *             encodeType: "ASCII"
+ *      responses:
+ *        "200":
+ *          description: Input was successfully appended to the list
+ */
 router.post('/singleUpdate', (req, res, next) => {
   const input = req.body.input;
-  if(req.body.encodeType) encodeType = req.body.encodeType;
+  if (req.body.encodeType) encodeType = req.body.encodeType;
 
   oprf.ready.then(function () {
     const key = oprf.hashToPoint(process.env.KEY);
@@ -40,13 +107,51 @@ router.post('/singleUpdate', (req, res, next) => {
 
     const body = csvStringifier.stringifyRecords(dataToWrite);
     fs.appendFileSync(fileName, header + body);
-    res.status(200).send('Complete!');
+    res.status(200).send('Single entry successfully added!');
   });
 });
 
+/**
+ * @swagger
+ * path:
+ *  /listholder/objectUpdate:
+ *    post:
+ *      summary: Add many SSNs passed in objects to the list
+ *      tags: [Update List]
+ *      parameters:
+ *       - in: body
+ *         name: Object-Update
+ *         schema:
+ *           type: object
+ *           required:
+ *            - input
+ *           properties:
+ *             input:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 required:
+ *                   - ssn
+ *                 properties:
+ *                   ssn:
+ *                     type: string
+ *                   name:
+ *                     type: string
+ *                   address:
+ *                     type: string
+ *             encodeType:
+ *               type: string
+ *               description: Whether to encode data being stored in 'ASCII' or 'UTF-8'
+ *           example:
+ *             input: [ { ssn: "123456789", name: "Billy Williamson", address: "123 Second St" }, { ssn: "123456790", name: "James Jameson", address: "124 Second St" } ]
+ *             encodeType: "ASCII"
+ *      responses:
+ *        "200":
+ *          description: Entries were successfully appended to the list
+ */
 router.post('/objectUpdate', (req, res, next) => {
   const input = req.body.input;
-  if(req.body.encodeType) encodeType = req.body.encodeType;
+  if (req.body.encodeType) encodeType = req.body.encodeType;
 
   oprf.ready.then(function () {
     const key = oprf.hashToPoint(process.env.KEY);
@@ -68,9 +173,39 @@ router.post('/objectUpdate', (req, res, next) => {
   });
 });
 
+/**
+ * @swagger
+ * path:
+ *  /listholder/arrayUpdate:
+ *    post:
+ *      summary: Add many SSNs passed in an array to the list
+ *      tags: [Update List]
+ *      parameters:
+ *       - in: body
+ *         name: Array-Update
+ *         schema:
+ *           type: object
+ *           required:
+ *            - input
+ *           properties:
+ *             input:
+ *               type: array
+ *               items:
+ *                 type: string
+ *                 description: SSN to be appended to active list
+ *             encodeType:
+ *               type: string
+ *               description: Whether to encode data being stored in 'ASCII' or 'UTF-8'
+ *           example:
+ *             input: [ "123456789", "987654321", "443256598" ]
+ *             encodeType: "ASCII"
+ *      responses:
+ *        "200":
+ *          description: Entries were successfully appended to the list
+ */
 router.post('/arrayUpdate', (req, res, next) => {
   const input = req.body.input;
-  if(req.body.encodeType) encodeType = req.body.encodeType;
+  if (req.body.encodeType) encodeType = req.body.encodeType;
 
   oprf.ready.then(function () {
     const key = oprf.hashToPoint(process.env.KEY);
@@ -92,8 +227,58 @@ router.post('/arrayUpdate', (req, res, next) => {
   });
 });
 
+/**
+ * @swagger
+ * tags:
+ *   name: List Data
+ *   description: Handling requests to the list holder
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /listholder/raiseToKey:
+ *    get:
+ *      summary: Raises all values passed to list holder's key and returns them
+ *      tags: [List Data]
+ *      parameters:
+ *       - in: body
+ *         name: Raise-Data
+ *         schema:
+ *           type: object
+ *           required:
+ *            - input
+ *            - secret
+ *           properties:
+ *             input:
+ *               type: array
+ *               items:
+ *                 type: string
+ *                 description: Values that have been raised to querier's key to be raised to holder's key
+ *             secret:
+ *               type: string
+ *               description: Shared secret between list holder and querier
+ *           example:
+ *             input: [ '*Û\nå¨T¾ô;g\r×ÛDð!½\u0017Xú³\u0005${Ì\f', '\u001cW·2ªíáY¶,3hMë¢Þ×', '¬Æ¾sÉxÖªCÈYù\u0018(\r\u001c\u0017Ý\u00194\f ¡ØÉÉA\u0007\u000b', '\u0007þ\u0005 .è¬îù¡ÕMæà&¤Î©k\f\u0013xo ', 'læÿ\u00122\u0003U!ÝøJ5¯ånÔ\u0019¶üT6±Xy¿-\t' ]
+ *             secret: "9201839012"
+ *      responses:
+ *        "403":
+ *          description: Incorrect shared key passed in
+ *        "200":
+ *          description: Entries were successfully appended to the list
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  input:
+ *                    type: array
+ *                    items:
+ *                      type: string
+ *                      description: values raised to both the querier's and holder's key
+ */
 router.get('/raiseToKey', (req, res, next) => {
-  if(req.body.secret != process.env.SHARED){
+  if (req.body.secret != process.env.SHARED) {
     res.status(403).send("Error 403: Incorrect shared secret value");
   } else {
     const input = req.body.input;
@@ -110,8 +295,45 @@ router.get('/raiseToKey', (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * path:
+ *  /listholder/listdata:
+ *    get:
+ *      summary: Sends back all of the entries contained in the active list
+ *      tags: [List Data]
+ *      parameters:
+ *       - in: body
+ *         name: Data-Request
+ *         schema:
+ *           type: object
+ *           required:
+ *            - secret
+ *           properties:
+ *             secret:
+ *               type: string
+ *               description: Shared secret between list holder and querier
+ *           example:
+ *             secret: "9201839012"
+ *      responses:
+ *        "403":
+ *          description: Incorrect shared key passed in
+ *        "200":
+ *          description: Entries were successfully appended to the list
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  input:
+ *                    type: array
+ *                    items:
+ *                      type: string
+ *                      description: Entries stored in the list
+ *
+ */
 router.get('/listdata', (req, res, next) => {
-  if(req.body.secret != process.env.SHARED) {
+  if (req.body.secret != process.env.SHARED) {
     res.status(403).send("Error 403: Incorrect shared secret value");
   } else {
     const tableData = ingest.readCsv(fileName);
@@ -122,11 +344,6 @@ router.get('/listdata', (req, res, next) => {
 
     res.status(200).json(result);
   }
-});
-
-router.put('/listname/:fileName', (req, res, next) => {
-  fileName = req.params.fileName + '.csv';
-  res.status(200).send("File changed to " + fileName + " successfully");
 });
 
 module.exports = router;
--- p2p/routes/query-list.js
+++ p2p/routes/query-list.js
@@ -8,7 +8,7 @@ const OPRF = require('oprf');
 // Set to ASCII encoding by default
 let encodeType = 'ASCII';
 let domain = 'localhost:3000';
-let holderDomain = 'localhost:8080';
+let holderDomain = 'localhost:3000';
 const oprf = new OPRF();
 
 router.put('/setparams', (req, res, next) => {
@@ -19,6 +19,61 @@ router.put('/setparams', (req, res, next) => {
   res.status(200).send("Encoding set to " + encodeType + ", domain set to " + domain + ", and holder domain set to " + holderDomain);
 });
 
+/**
+ * @swagger
+ * tags:
+ *   name: Query List
+ *   description: Extracting information from another list holder
+ */
+
+/**
+ * @swagger
+ * path:
+ *  /querylist/maskWithHolderKey:
+ *    get:
+ *      summary: Masks with querier's key and sends those values to be masked by the list holder
+ *      tags: [Query List]
+ *      parameters:
+ *       - in: body
+ *         name: Query-Array
+ *         schema:
+ *           type: object
+ *           required:
+ *            - input
+ *            - key
+ *            - secret
+ *           properties:
+ *             input:
+ *               type: array
+ *               items:
+ *                 type: string
+ *               description: Plaintext data to be masked and sent to list holder
+ *             key:
+ *               type: string
+ *               description: Key to raise data to before sending to list holder
+ *             secret:
+ *               type: string
+ *               description: Secret value shared between querier and list holder
+ *           example:
+ *             input: [ "748320512", "002381635", "129427809", ... ]
+ *             key: "56423091200"
+ *             secret: "23449023"
+ *      responses:
+ *        "200":
+ *          description: Entries were successfully appended to the list
+ *          content:
+ *            application/json:
+ *              schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                     description: values raised to both the querier's and holder's key
+ *               example:
+ *                 result: [ "2341231" ]
+ */
 router.get('/maskWithHolderKey', (req, res, next) => {
   const input = req.body.input;
   let key = req.body.key;
@@ -31,6 +86,11 @@ router.get('/maskWithHolderKey', (req, res, next) => {
       return oprf.encodePoint(maskedValue, encodeType);
     });
 
+    console.log("Input: " + input);
+
+    console.log("Data: ");
+    console.log(data);
+
     var options = {
       method: 'GET',
       url: 'http://' + holderDomain + '/listholder/raiseToKey',
@@ -38,14 +98,10 @@ router.get('/maskWithHolderKey', (req, res, next) => {
       {
         'cache-control': 'no-cache',
         Connection: 'keep-alive',
-        //'Content-Length': '99',
         Host: holderDomain,
-        'Postman-Token': 'fc1f1da5-31d4-46b3-a004-a4652933ff5d,d058b93b-e7a8-43c0-b1fd-690ba3b4fd73',
         'Cache-Control': 'no-cache',
         Accept: '*/*',
-        'User-Agent': 'PostmanRuntime/7.20.1',
         'Content-Type': 'application/json',
-        Authorization: 'Bearer 6AQnIyMC6jUeNkE4eGvqW7BSOpdLrEvp4bIgmmsN_3Vgc8tABwNQ9QHmsMavZ5Z9ZWzgoLrx30tCEXxZyLFxtbUZlBmSFrYWxhOZspIXByZJoCC-geQi1fkAXCCuXXYx'
       },
       body:
       {
@@ -81,12 +137,9 @@ router.get('/getAndDecodeTable', async (req, res, next) => {
         'cache-control': 'no-cache',
         Connection: 'keep-alive',
         Host: holderDomain,
-        'Postman-Token': '46b7e7e1-a310-4a8e-9f9c-84adcb772599,a317a329-fbcd-46d7-9765-a704171899a1',
         'Cache-Control': 'no-cache',
         Accept: '*/*',
-        'User-Agent': 'PostmanRuntime/7.20.1',
         'Content-Type': 'application/json',
-        Authorization: 'Bearer 6AQnIyMC6jUeNkE4eGvqW7BSOpdLrEvp4bIgmmsN_3Vgc8tABwNQ9QHmsMavZ5Z9ZWzgoLrx30tCEXxZyLFxtbUZlBmSFrYWxhOZspIXByZJoCC-geQi1fkAXCCuXXYx'
       },
       body: { secret: secret },
       json: true
@@ -104,6 +157,49 @@ router.get('/getAndDecodeTable', async (req, res, next) => {
   });
 });
 
+/**
+ * @swagger
+ * path:
+ *  /querylist/checkIfInList:
+ *    get:
+ *      summary: Checks if entries in input are entries in a list held by another listholder and returns indexes of those that are
+ *      tags: [Query List]
+ *      parameters:
+ *       - in: body
+ *         name: Query-Array
+ *         schema:
+ *           type: object
+ *           required:
+ *            - input
+ *            - secret
+ *           properties:
+ *             input:
+ *               type: array
+ *               items:
+ *                 type: string
+ *               description: Key to raise data to before sending to list holder
+ *             secret:
+ *               type: string
+ *               description: Secret value shared between querier and list holder
+ *           example:
+ *             input: [ "748320512", "002381635", "129427809", ... ]
+ *             secret: "23449023"
+ *      responses:
+ *        "200":
+ *          description: Entries were successfully appended to the list
+ *          content:
+ *            application/json:
+ *              schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: array
+ *                   items:
+ *                     type: int
+ *                     description: indexes of input values that were found in the other list
+ *               example:
+ *                 result: [ 9, 43, 210, 311 ]
+ */
 router.get('/checkIfInList', (req, res, next) => {
   const input = req.body.input;
   const secret = req.body.secret;
@@ -120,12 +216,9 @@ router.get('/checkIfInList', (req, res, next) => {
         'cache-control': 'no-cache',
         Connection: 'keep-alive',
         Host: domain,
-        'Postman-Token': '46b7e7e1-a310-4a8e-9f9c-84adcb772599,49759aa3-1c34-4198-970e-ec60f594fbf6',
         'Cache-Control': 'no-cache',
         Accept: '*/*',
-        'User-Agent': 'PostmanRuntime/7.20.1',
         'Content-Type': 'application/json',
-        Authorization: 'Bearer 6AQnIyMC6jUeNkE4eGvqW7BSOpdLrEvp4bIgmmsN_3Vgc8tABwNQ9QHmsMavZ5Z9ZWzgoLrx30tCEXxZyLFxtbUZlBmSFrYWxhOZspIXByZJoCC-geQi1fkAXCCuXXYx'
       },
       body:
       {
